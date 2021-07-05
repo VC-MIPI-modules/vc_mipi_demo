d8b68a8e919f12f2a9f161855dcf660f1348288a
pmliquify <peter.martienssen@liquify-consulting.de>
2021-07-05 19:02:16 +0200

    Added a script to copy binaries to the target device.

---
+++ bin/flash.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 [options]"
+	echo ""
+	echo "Flash all tools to the target."
+	echo ""
+	echo "Supported options:"
+        echo "-a, --all                 Flash all"
+        echo "-h, --help                Show this help text"
+}
+
+configure() {
+        . config/configure.sh
+}
+
+flash_all() {
+        echo "Flash all ..."
+        $TARGET_SHELL mkdir -p /home/$TARGET_USER/test
+        scp $SRC_DIR/vcmipidemo $TARGET_USER@$TARGET_IP:/home/$TARGET_USER/test
+        scp $SRC_DIR/vcimgnetsrv $TARGET_USER@$TARGET_IP:/home/$TARGET_USER/test
+}
+
+while [ $# != 0 ] ; do
+	option="$1"
+	shift
+
+	case "${option}" in
+	-a|--all)
+		configure
+                flash_all
+                exit 0
+		;;
+	-h|--help)
+		usage
+		exit 0
+		;;
+	*)
+		echo "Unknown option ${option}"
+		exit 1
+		;;
+	esac
+done
+
+usage
+exit 1
