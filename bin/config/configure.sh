4575a07a134217e37d8a62d395f201c6e9b5fc83
pmliquify <peter.martienssen@liquify-consulting.de>
2021-07-05 19:07:07 +0200

    Added TARGET configuration variables.

--- bin/config/configure.sh
+++ bin/config/configure.sh
@@ -11,3 +11,7 @@ export CROSS_COMPILE=aarch64-none-linux-gnu-
 export PATH=$BUILD_DIR/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/:$PATH
 export DTC_FLAGS="-@"
 export ARCH=arm64
+
+TARGET_USER=root
+TARGET_IP=
+TARGET_SHELL="ssh $TARGET_USER@$TARGET_IP"
