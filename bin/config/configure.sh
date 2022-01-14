c769aec739b4e7dc89525a1aa8c4c9ae278d6497
Peter Martienssen <peter.martienssen@liquify-consulting.de>
2022-01-14 17:22:52 +0100

    Moved CC environment variable from Makefile to configure.sh script

--- bin/config/configure.sh
+++ bin/config/configure.sh
@@ -7,11 +7,10 @@ BUILD_DIR=$WORKING_DIR/build
 SRC_DIR=$WORKING_DIR/src
 DOWNLOADS_DIR=$BUILD_DIR/downloads
 
-export CROSS_COMPILE=aarch64-none-linux-gnu-
 export PATH=$BUILD_DIR/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/:$PATH
 export DTC_FLAGS="-@"
-export ARCH=arm64
+export CC=aarch64-none-linux-gnu-gcc
 
 TARGET_USER=root
-TARGET_IP=
+TARGET_IP=192.168.2.37
 TARGET_SHELL="ssh $TARGET_USER@$TARGET_IP"
